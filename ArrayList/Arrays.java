4748dc8a995f2478d7b0d74463ba8ebd2ce02b19
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-05-22 17:03:10 +0530

    Initial commit

--- ArrayList/Arrays.java
+++ ArrayList/Arrays.java
@@ -1,5 +1,23 @@
 package ArrayList;
 
-public class Arrays {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Arrays {
+    public static void main(String[] args) {
+
+        ArrayList <Integer> List = new ArrayList();
+
+        List.add(3);
+        List.add(2);
+        List.add(1);
+
+        //remove value
+        List.remove(Integer.valueOf(1));
+        System.out.println(List);
+
+        //collection
+        Collections.sort(List);
+        System.out.println(List);
+    }
 }
