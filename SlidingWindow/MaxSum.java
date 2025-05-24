77449106227c71ff022903959565e298eb8df9f6
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-05-24 19:02:46 +0530

    Initial commit

--- SlidingWindow/MaxSum.java
+++ SlidingWindow/MaxSum.java
@@ -4,26 +4,24 @@ public class MaxSum {
     public static int max(int[] arr, int k) {
 
         int n = arr.length;
-        int sum = 0;
-        int maxsum = sum;
-
-
         if (k > n){
             return -1;
         }
 
         //sum
+        int sum = 0;
         for (int i=0; i<k; i++){
             sum += arr[i];
         }
 
         //windows
+        int maxsum = sum;
         for (int i=k; i<arr.length; i++){
             sum = sum + arr[i] - arr[i - k];
-            sum = Math.max(sum, maxsum);
+            maxsum = Math.max(maxsum, sum);
         }
 
-        return sum;
+        return maxsum;
     }
 
     public static void main(String[] args) {
@@ -32,4 +30,6 @@ public class MaxSum {
         System.out.println(max(arr, k));
     }
 }
+
+
 
