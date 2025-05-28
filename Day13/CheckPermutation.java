665378c24e5cbb19d67e0a638a3127877ded8957
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-05-28 10:31:22 +0530

    Initial commit

--- Day13/CheckPermutation.java
+++ Day13/CheckPermutation.java
@@ -1,9 +1,32 @@
 package Day13;
 
+import java.util.Arrays;
+
 public class CheckPermutation {
     public static boolean search(String txt , String pat ) {
+        int n = txt.length();
+        int m = pat.length();
 
-        String first = new String()
+        int[] patFreq = new int[26];
+        int[] windows = new int[26];
+
+        // Count frequencies of pat and first window of txt
+        for (int i=0; i<m; i++){
+            patFreq[pat.charAt(i) - 'a']++;
+            windows[txt.charAt(i) - 'a']++;
+        }
+
+        // Slide the window over txt
+        for (int i=m; i<n; i++){
+            windows[txt.charAt(i-m) - 'a']--;
+            windows[txt.charAt(i)- 'a']++;
+
+            if (Arrays.equals(patFreq, windows)){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
