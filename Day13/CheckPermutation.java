e2bcdcf0bd78b1c78f862424af9afe3cacaad689
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-05-28 10:03:42 +0530

    Initial commit

---
+++ Day13/CheckPermutation.java
@@ -0,0 +1,15 @@
+package Day13;
+
+public class CheckPermutation {
+    public static boolean search(String txt , String pat ) {
+
+        String first = new String()
+    }
+
+    public static void main(String[] args) {
+        String txt = "geeks";
+        String pat = "eke";
+
+        System.out.println(search(txt, pat));
+    }
+}
--- SlidingWindow/Smallest01and2.java
+++ SlidingWindow/Smallest01and2.java
@@ -1,37 +1,44 @@
-//package SlidingWindow;
-//
-//public class Smallest01and2 {
-//    public static int smallestSubstring(String S) {
-//
-//        int count = Integer.MAX_VALUE;
-//        boolean zero = false;
-//        boolean one = false;
-//        boolean two = false;
-//
-//        int zeroindex = 0;
-//        int oneindex = 0;
-//        int twoindex = 0;
-//
-//
-//        for (int i=0; i<S.length(); i++){
-//            if (S.charAt(i) == '0'){
-//                zero = true;
-//                zeroindex = i;
-//            }
-//            else if (S.charAt(i) == '1') {
-//                one = true;
-//                oneindex = i;
-//            }
-//            else if (S.charAt(i) == '2') {
-//                two = true;
-//                twoindex = i;
-//            }
-//        }
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        String S = "1O212";
-//        System.out.println(smallestSubstring(S));
-//    }
-//}
+package SlidingWindow;
+
+public class Smallest01and2 {
+    public static int smallestSubstring(String S) {
+
+        int count = Integer.MAX_VALUE;
+        boolean zero = false;
+        boolean one = false;
+        boolean two = false;
+
+        int zeroindex = -1;
+        int oneindex = -1;
+        int twoindex = -1;
+
+
+        for (int i=0; i<S.length(); i++){
+            if (S.charAt(i) == '0'){
+                zero = true;
+                zeroindex = i;
+            }
+            else if (S.charAt(i) == '1') {
+                one = true;
+                oneindex = i;
+            }
+            else if (S.charAt(i) == '2') {
+                two = true;
+                twoindex = i;
+            }
+
+            if(zero && one && two){
+                int Max = Math.max(zeroindex, Math.max(oneindex, twoindex));
+                int Min = Math.min(zeroindex, Math.min(oneindex,twoindex));
+
+                count = Math.min(count, Max - Min + 1);
+            }
+        }
+         return count == Integer.MAX_VALUE ? -1 : count;
+    }
+
+    public static void main(String[] args) {
+        String S = "10212";
+        System.out.println(smallestSubstring(S));
+    }
+}
